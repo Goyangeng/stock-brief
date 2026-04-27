6fd4c232f9c93dec2b83c1c84b853cb1d0a7b891
Goyangeng <120919606+Goyangeng@users.noreply.github.com>
2026-04-27 20:23:27 +0900

    chore: initial project scaffold (Spring Boot 3.5 + Kotlin + PostgreSQL Docker)

---
+++ src/main/kotlin/com/goyk/stockbrief/StockBriefApplication.kt
@@ -0,0 +1,11 @@
+package com.goyk.stockbrief
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockBriefApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockBriefApplication>(*args)
+}
---
+++ src/test/kotlin/com/goyk/stockbrief/StockBriefApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.goyk.stockbrief
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockBriefApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
